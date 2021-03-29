e67304a0f66535e4df61b3ebeed85e3f097bb998
Matt Kane <matt@gatsbyjs.com>
2021-03-29 12:58:06 +0100

    Stub out search page

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,20 @@
 import * as React from 'react'
 import { StoreProvider } from './src/context/store-context'
 import './src/styles/variables.css'
+import { createClient, Provider as UrlqProvider } from 'urql'
+
+const client = createClient({
+  url: `https://${process.env.GATSBY_SHOPIFY_STORE_URL}/api/2021-01/graphql.json`,
+  fetchOptions: {
+    headers: {
+      'X-Shopify-Storefront-Access-Token':
+        process.env.GATSBY_STOREFRONT_ACCESS_TOKEN,
+    },
+  },
+})
 
 export const wrapRootElement = ({ element }) => (
-  <StoreProvider>{element}</StoreProvider>
+  <UrlqProvider value={client}>
+    <StoreProvider>{element}</StoreProvider>
+  </UrlqProvider>
 )
--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,10 +1,14 @@
 import * as React from 'react'
-import { graphql, Link, useStaticQuery } from 'gatsby'
-import { SearchSidebar } from '../components/search-sidebar'
+import { graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
+import { CheckFilter } from '../components/check-filter'
+import { useProductSearch } from '../utils/hooks'
+import { main, search, results, filters } from './search-page.module.css'
 
 const SearchPage = () => {
-  const data = useStaticQuery(graphql`
+  const {
+    products: { productTypes, vendors, tags },
+  } = useStaticQuery(graphql`
     query {
       products: allShopifyProduct {
         productTypes: distinct(field: productType)
@@ -14,9 +18,69 @@ const SearchPage = () => {
     }
   `)
 
+  const [selectedTags, setSelectedTags] = React.useState(tags)
+  const [selectedVendors, setSelectedVendors] = React.useState(vendors)
+  const [selectedProductTypes, setSelectedProductTypes] = React.useState(
+    productTypes
+  )
+
+  const [searchTerm, setSearchTerm] = React.useState('')
+
+  const [{ data, fetching, ...props }] = useProductSearch(
+    {
+      term: searchTerm,
+      selectedTags,
+      selectedProductTypes,
+      selectedVendors,
+      allProductTypes: productTypes,
+      allVendors: vendors,
+      allTags: tags,
+    },
+    `RELEVANCE`
+  )
+  console.log(props)
   return (
     <Layout>
-      <SearchSidebar {...data.products} />
+      <div className={main}>
+        <div className={search}>
+          <input
+            type="search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.currentTarget.value)}
+            placeholder="Search..."
+          />
+        </div>
+        <section className={filters}>
+          <span>{fetching ? 'Loading...' : ' '}</span>
+
+          <CheckFilter
+            name="Type"
+            items={productTypes}
+            selectedItems={selectedProductTypes}
+            setSelectedItems={setSelectedProductTypes}
+          />
+          <CheckFilter
+            name="Brands"
+            items={vendors}
+            selectedItems={selectedVendors}
+            setSelectedItems={setSelectedVendors}
+          />
+          <CheckFilter
+            open={false}
+            name="Tags"
+            items={tags}
+            selectedItems={selectedTags}
+            setSelectedItems={setSelectedTags}
+          />
+        </section>
+        <section className={results}>
+          <ul>
+            {data?.products?.edges?.map(({ node }) => (
+              <li>{node.title}</li>
+            ))}
+          </ul>
+        </section>
+      </div>
     </Layout>
   )
 }
