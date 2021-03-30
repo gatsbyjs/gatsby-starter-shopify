a6cf7b3cbe0409c5b8ecb0b535a5df9d1a686d51
Matt Kane <matt@gatsbyjs.com>
2021-03-30 15:02:21 +0100

    Move urql provider into StoreProvider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,20 +1,7 @@
 import * as React from 'react'
 import { StoreProvider } from './src/context/store-context'
 import './src/styles/variables.css'
-import { createClient, Provider as UrlqProvider } from 'urql'
-
-const client = createClient({
-  url: `https://${process.env.GATSBY_SHOPIFY_STORE_URL}/api/2021-01/graphql.json`,
-  fetchOptions: {
-    headers: {
-      'X-Shopify-Storefront-Access-Token':
-        process.env.GATSBY_STOREFRONT_ACCESS_TOKEN,
-    },
-  },
-})
 
 export const wrapRootElement = ({ element }) => (
-  <UrlqProvider value={client}>
-    <StoreProvider>{element}</StoreProvider>
-  </UrlqProvider>
+  <StoreProvider>{element}</StoreProvider>
 )
