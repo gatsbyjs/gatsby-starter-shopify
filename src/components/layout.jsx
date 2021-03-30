a4782444574eda2eacb41f153cdfecfcdfc7bdea
Ekwuno Obinna <obinnacodes@gmail.com>
2021-03-30 16:42:04 +0400

    Revert "rebuilt homepage with css modules to fit design"
    This reverts commit 72dfc4548c0fb025463f6fbab54e0bd88faf0627.

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,16 +1,18 @@
 import * as React from 'react'
-// import { SkipNavContent, SkipNavLink } from './skip-nav'
+import { SkipNavContent, SkipNavLink } from './skip-nav'
 import Header from './header'
 import Footer from './footer'
 import SEO from './seo'
-// import { ChakraHelpersProvider } from '../context/chakra-helpers-context'
+import { ChakraHelpersProvider } from '../context/chakra-helpers-context'
 
 const Layout = ({ children }) => (
-  <div>
+  <ChakraHelpersProvider>
+    <SEO />
+    <SkipNavLink />
     <Header />
-    {children}
+    <SkipNavContent>{children}</SkipNavContent>
     <Footer />
-  </div>
+  </ChakraHelpersProvider>
 )
 
 export default Layout
