1cc9196c235623252ec04c7094e05fc45e05c7b1
Matt Kane <matt@gatsbyjs.com>
2021-04-20 10:00:49 +0100

    Responsive header

--- src/components/cart-button.jsx
+++ src/components/cart-button.jsx
@@ -3,9 +3,9 @@ import { cartButtonContainerStyle, badgeStyle } from './cart-button.module.css'
 import { CgShoppingBag } from 'react-icons/cg'
 import { Link } from 'gatsby'
 
-const CartButton = ({ quantity, btnRef, onOpen }) => {
+const CartButton = ({ quantity, className }) => {
   return (
-    <div className={cartButtonContainerStyle}>
+    <div className={[cartButtonContainerStyle, className].join(' ')}>
       <Link aria-label={`Shopping Cart with ${quantity} items`} to="/cart">
         <CgShoppingBag size={24} />
       </Link>
