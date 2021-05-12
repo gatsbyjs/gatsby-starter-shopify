911cc22704d51c1b3d8b71e073c3944bd9f8fa33
jxnblk <jxnblk@gmail.com>
2021-05-12 16:17:48 -0400

    Add Google Analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     siteTitle: "gatsby-starter-shopify",
     siteTitleDefault: "gatsby-starter-shopify by @GatsbyJS",
-    siteUrl: "https://shopify30k.gatsbyjs.io",
+    siteUrl: "https://shopify-demo.gatsbyjs.com",
     hrefLang: "en",
     siteDescription:
       "A Gatsby starter using the latest Shopify plugin showcasing a store with product overview, individual product pages, and a cart.",
@@ -30,5 +30,13 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-gatsby-cloud",
-  ],
+    // Add your Google Analytics ID to the .env file to enable
+    // Otherwise, this plugin can be removed
+    process.env.GOOGLE_ANALYTICS_ID && {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+  ].filter(Boolean),
 }
