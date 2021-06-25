f045cd4fccb9a9a35b3f42c371e47792b96bd31e
Brent Jackson <jxnblk@gmail.com>
2021-06-25 18:14:59 -0400

    Add conditional logic to hide deploy button when not deployed as the demo site (#32)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -19,28 +19,38 @@ export const query = graphql`
     }
   }
 `
+function Hero (props) {
+  return (
+    <div className={container}>
+      <h1 className={intro}>Welcome to the GatsbyJS + Shopify Demo Store.</h1>
+      {!!process.env.GATSBY_DEMO_STORE && (
+        <>
+          <p className={callOut}>
+            It's a proof-of-concept in a box, with 10k products and 30k variants
+            to help you get to proof-of-concept as soon as right now.
+          </p>
+          <p className={callToAction}>
+            Hook it up to your own Shopify store data and start customizing in
+            minutes by deploying it to Gatsby Cloud for free. Grab your Shopify
+            store credentials and
+            <a href="https://www.gatsbyjs.com/dashboard/deploynow?url=https://github.com/gatsbyjs/gatsby-starter-shopify&utm_campaign=shopify-starter">
+              <img
+                src="https://www.gatsbyjs.com/deploynow.png"
+                alt="Deploy to Gatsby Cloud"
+                className={deployButton}
+              />
+            </a>
+          </p>
+        </>
+      )}
+    </div>
+  )
+}
+
 export default function IndexPage({ data }) {
   return (
     <Layout>
-      <div className={container}>
-        <h1 className={intro}>Welcome to the GatsbyJS + Shopify Demo Store.</h1>
-        <p className={callOut}>
-          It's a proof-of-concept in a box, with 10k products and 30k variants
-          to help you get to proof-of-concept as soon as right now.
-        </p>
-        <p className={callToAction}>
-          Hook it up to your own Shopify store data and start customizing in
-          minutes by deploying it to Gatsby Cloud for free. Grab your Shopify
-          store credentials and
-          <a href="https://www.gatsbyjs.com/dashboard/deploynow?url=https://github.com/gatsbyjs/gatsby-starter-shopify&utm_campaign=shopify-starter">
-            <img
-              src="https://www.gatsbyjs.com/deploynow.png"
-              alt="Deploy to Gatsby Cloud"
-              className={deployButton}
-            />
-          </a>
-        </p>
-      </div>
+      <Hero />
       <ProductListing products={data?.shopifyCollection?.products} />
     </Layout>
   )
