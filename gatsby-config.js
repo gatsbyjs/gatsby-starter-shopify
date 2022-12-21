4865d8533984f78080d9fdb041a10fdc3e647847
whitneymeredith <97134315+whitneymeredith@users.noreply.github.com>
2022-12-21 17:45:51 -0600

    remove export modules from gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,10 +38,4 @@ module.exports = {
   ].filter(Boolean),
 }
 
-module.exports = {
-  plugins: [
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`, // Needed for dynamic images
-  ],
-}
+
